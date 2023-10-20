a9cbf54a6361b8475f9825e0a39a34641586078f
jay <yates.j.a@outlook.com>
2023-10-19 22:18:35 -0400

    Add utils module to gh

---
+++ utils/src/test/java/org/yates/io/InputHandler.java
@@ -0,0 +1,59 @@
+package org.yates.io;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class InputHandlerTest {
+
+    @AfterEach
+    void reset() {
+        System.setIn(System.in);
+    }
+
+    @Test
+    void query() throws IOException {
+        // Creating mock input stream
+        BufferedReader mockReader = mock(BufferedReader.class);
+        when(mockReader.readLine())
+                .thenReturn("y")
+                .thenReturn("n");
+
+
+        InputHandler inputHandler = InputHandler.builder()
+                .validator(in -> in.equals("y") || in.equals("n"))
+                .prompt("Do you like cookies with milk? (Y/n): ")
+                .errorMessage("Please reply with 'Y' or 'N'")
+                .setReader(mockReader)
+                .build();
+        assertEquals("y", inputHandler.query());
+
+        inputHandler.setPrompt("Do you like oranges? (Y/n): ");
+        assertEquals("n", inputHandler.query());
+    }
+
+    @Test
+    void queryWords() throws IOException {
+        BufferedReader mockReader = mock(BufferedReader.class);
+        when(mockReader.readLine())
+                .thenReturn("Portugal")
+                .thenReturn("Washington State")
+                .thenReturn("Washington DC");
+
+
+        InputHandler inputHandler = InputHandler.builder()
+                .validator(in -> in.equals("washington dc"))
+                .prompt("What's the capitol of the USA? ")
+                .errorMessage("Incorrect!")
+                .setReader(mockReader)
+                .build();
+
+        assertEquals("washington dc", inputHandler.query());
+    }
+}
